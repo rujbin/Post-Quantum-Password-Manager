85a255eb782d35557fcc837b26b01b54daf99eea
rujbin <115537871+rujbin@users.noreply.github.com>
2024-11-06 13:48:06 +0100

    Delete src directory

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-# Versionsinformationen
-__version__ = "1.0.0"
-__author__ = "fSN"
-__email__ = "umpire@tutamail.com"
-
-from .core.password_manager import PasswordManager
-from .ui.main_window import MainWindow
